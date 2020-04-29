769b190436165e767f365c86b52f535191e7f886
bnbasilio <bnbasilio@gmail.com>
2020-04-29 04:29:54 +0000

    added string formats

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-for i in range(8):
-    for j in range(1, 9):
+for i in range(9):
+    for j in range(1, 10):
         if i == 0 and j == 1:
-            print("01", end='')
+            print("{:d}{:d}".format(i, j), end='')
         elif j > i:
             print(", {:d}{:d}".format(i, j), end='')
-print(", 89")
+        elif i == 8 and j == 9:
+            print(", {:d}{:d}".format(i, j))
