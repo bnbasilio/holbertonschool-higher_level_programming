d681ce8bf5ded8f8c92e8c205e8a3cc059e5dae0
bnbasilio <bnbasilio@gmail.com>
2020-04-29 04:42:55 +0000

    changed len strategy to abs instead

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    strnum = str(number)
-    print(strnum[len(strnum) - 1], end='')
-    return(strnum[len(strnum) - 1])
+    last = abs(number) % 10
+    print("{:d}".format(last), end='')
+    return(last)
