76fd72fc924063d8806c65459fd51c18e31c07a2
bnbasilio <bnbasilio@gmail.com>
2020-04-29 05:18:21 +0000

    change len strategy to abs instead

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-strnum = str(number)
-if number > 0:
-    last = int(strnum[len(strnum)-1])
-elif number < 0:
-    last = int(strnum[len(strnum)-1]) * -1
+last = abs(number) % 10
+if number < 0:
+    last *= -1
+print("Last digit of {:d} is {:d} and is ".format(number, last), end='')
 if last > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5"
-          .format(number, last))
+    print("greater than 5")
 elif last == 0:
-    print("Last digit of {:d} is {:d} and is 0".format(number, last))
+    print("0")
 elif last < 6 and last != 0:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
-          .format(number, last))
+    print("less than 6 and not 0")
