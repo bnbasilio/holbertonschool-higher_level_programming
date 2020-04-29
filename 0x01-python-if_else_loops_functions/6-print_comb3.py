b898ce6c0082bfbb9d35d2f312c3e494bcd621b8
bnbasilio <bnbasilio@gmail.com>
2020-04-29 05:24:17 +0000

    re-arranged conditionals

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for i in range(9):
-    for j in range(1, 10):
-        if i == 0 and j == 1:
-            print("{:d}{:d}".format(i, j), end='')
-        elif j > i:
-            print(", {:d}{:d}".format(i, j), end='')
-        elif i == 8 and j == 9:
-            print(", {:d}{:d}".format(i, j))
+for i in range(10):
+    for j in range(10):
+        if j > i:
+            if i == 8 and j == 9:
+                print("{:d}{:d}".format(i, j))
+            else:
+                print("{:d}{:d}, ".format(i, j), end='')
