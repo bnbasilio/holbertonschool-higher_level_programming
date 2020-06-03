3dd9d4a37b6710d2ac5684b88b5f632aa6041ef3
bnbasilio <bnbasilio@gmail.com>
2020-06-03 19:52:13 +0000

    added documentations

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""0-read_file module"""
 
 def read_file(filename=""):
+    """reads a text file and prints it to stdout"""
     with open(filename, 'r') as f:
         print(f.read(), end="")
--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+"""1-number_of_lines module"""
 
 def number_of_lines(filename=""):
+    """returns the number of lines of a text file"""
     lines = 0
     with open(filename, 'r') as f:
         for line in f:
