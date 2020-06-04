f266cc14f96b6d6b074af1ecf0b312f5b8b54d04
bnbasilio <bnbasilio@gmail.com>
2020-06-04 05:07:05 +0000

    task 15 completed

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""100-append after module"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file, after each line containing a
+       specific string"""
+    if filename == "" or search_string == "" or new_string == "":
+        return filename
+
+    with open(filename) as f:
+        _list = list(f)
+
+    for i in range(len(_list)):
+        if search_string in _list[i]:
+            _list.insert(i + 1, new_string)
+
+    with open(filename, 'w') as f:
+        nf.writelines(_list)
+
+    return filename
