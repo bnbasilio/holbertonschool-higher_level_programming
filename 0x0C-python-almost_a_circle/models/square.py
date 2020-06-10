bcc3af2fc2bc0b03e5884951cb5e0528a146ca5f
bnbasilio <bnbasilio@gmail.com>
2020-06-10 00:08:52 +0000

    task 10 completed

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """a square has a size, x, y, and id"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """returns string representation of Square"""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.size)
