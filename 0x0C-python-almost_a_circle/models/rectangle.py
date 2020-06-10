c86bf7d3c5803d86d20f7a566a268dd859e73149
bnbasilio <bnbasilio@gmail.com>
2020-06-10 05:39:44 +0000

    fixed pep8 error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -90,6 +90,7 @@ class Rectangle(Base):
         for key, value in kwargs.items():
             if hasattr(self, key):
                 setattr(self, key, value)
+
     def to_dictionary(self):
         """returns the dictionary representation of a Rectangle"""
         return {"id": self.id, "width": self.__width, "height":
