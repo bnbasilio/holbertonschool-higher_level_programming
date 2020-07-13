7f20ab9f0a112ffa888cb5c32dd1a4974ffab124
bnbasilio <bnbasilio@gmail.com>
2020-07-13 22:37:04 +0000

    square brackets removed

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- creates the database hbtn_0c_0 in your MySQL server
 -- script does not fail if the database already exists
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
