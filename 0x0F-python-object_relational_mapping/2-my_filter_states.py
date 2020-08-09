0d154fe62242aa78bda4b3652289187cb77b7652
bnbasilio <bnbasilio@gmail.com>
2020-08-09 02:20:07 +0000

    task 2 completed

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""lists all states where name matches argument from database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE states.name LIKE '%%{arg}%%'  \
+                ORDER BY states.id ASC".format(arg=argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
