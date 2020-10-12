5c31a8840260e9eb83a014a9064bcd87011e6e77
Nic Basilio <bnbasilio@gmail.com>
2020-10-12 06:24:37 +0000

    all tasks for web scraping project

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// prints all characters of a Star Wars movie
+const request = require('request');
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+const getPerson = (charEndpoint) => {
+  request(charEndpoint, function (error, response, body) {
+    if (error) throw error;
+    console.log(JSON.parse(body).name);
+  });
+};
+request(url, function (error, response, body) {
+  if (error) throw error;
+  JSON.parse(body).characters.forEach(character => getPerson(character));
+});
+
